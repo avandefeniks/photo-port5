1935d37c132df667c164a1781d0fb06ad3c42d96
avandefeniks <angel.vandefeniks@gmail.com>
2023-10-17 14:46:42 -0700

    added about and nav components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-// import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
